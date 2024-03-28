185e868f292099fe7d85312c555769a997122aec
krbibi <bsy5372@naver.com>
2024-03-28 13:30:53 +0900

    test

--- backend/print/views.py
+++ backend/print/views.py
@@ -6,5 +6,5 @@ from rest_framework.views import APIView
 class HelloWorld(APIView):
 
     def get(self,request):
-        data = {'message': 'Hello World!abw'}
+        data = {'message': 'Hello World!qwe'}
         return Response(data)
