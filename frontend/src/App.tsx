7e9e4192c4e4884766b0c19cbd676909e216b108
hayeon <hayeon010904@gmail.com>
2024-03-31 20:53:56 +0900

    test 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>test1</h1>
+      <h1>test100</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
