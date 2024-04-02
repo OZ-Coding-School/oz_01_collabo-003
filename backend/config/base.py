a750c2e8b5166c23840a91f1fbcd679e1b3cae65
Fillsogood <jdd05273@naver.com>
2024-04-02 10:52:44 +0900

    feat:open ai 연결

--- backend/config/base.py
+++ backend/config/base.py
@@ -25,9 +25,10 @@ SECRET = json.loads(config_scret_common_str)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET["SECRET_KEY_NAME"]
-
+OPENAI_API_KEY = SECRET["OPENAI_API_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
