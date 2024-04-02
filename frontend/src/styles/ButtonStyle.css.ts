2088279806a20a0931521490a015705c4f9383a0
hayeon <hayeon010904@gmail.com>
2024-04-03 00:35:48 +0900

    ✨ feat : mainPage 디자인 및 애니메이션

--- frontend/src/styles/ButtonStyle.css.ts
+++ frontend/src/styles/ButtonStyle.css.ts
@@ -5,18 +5,31 @@ export const buttonStyle = style({
   // backgroundColor: vars.color.purple2 ,
   color: "#FFFFFF",
   backgroundColor: "#838ade9d",
-  fontSize: "2rem",
-  width: "28%",
+  fontSize: "2.5rem",
+  fontWeight: "bolder",
+  width: "18%",
+  padding: "0.3rem",
   height: "10%",
   border: "none",
   borderRadius: "50px",
   position: "absolute",
   cursor: "pointer",
+  transition: "box-shadow 0.3s",
 });
 export const mainButton = style([
   buttonStyle,
-  { left: "50%", transform: "translateX(-50%)", top: "75%" },
+  {
+    left: "50%",
+    transform: "translateX(-50%)",
+    top: "82%",
+    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+    transition: "all 0.3s cubic-bezier(.25,.8,.25,1);",
+    ":hover": {
+      boxShadow: "0 12px 18px rgba(0,0,0,0.25), 0 8px 1px rgba(0,0,0,0.22)",
+    },
+  },
 ]);
+
 export const formButton = style([
   buttonStyle,
   { left: "50%", transform: "translateX(-50%)", top: "75%" },
