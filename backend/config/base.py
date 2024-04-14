339e0ff381e1ff06fa92a67a1eb3b29c8a6823f4
Fillsogood <jdd05273@naver.com>
2024-04-14 11:17:25 +0900

    feat:비밀번호 찾기 장고 html 간단한 커스텀 추후에 이쁘게 꾸밀 예정

--- backend/config/base.py
+++ backend/config/base.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,6 +90,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path("register/", views.RegisterAPIView.as_view(), name='register'), #회원가입하기
     path("login/", views.LoginAPIView.as_view(), name='login'), #로그인하기
@@ -10,8 +9,8 @@ urlpatterns = [
     path("myinfo/", views.MyInfo.as_view(), name='myinfo'), #유저 정보 업데이트
     path("<int:user_id>/", views.GetUserDataAPIView.as_view(), name='detailinfo'), #특정 유저 질문, 답변, 피드백, 점수 가져오기
     path("passwordReset/", views.PasswordResetAPIView.as_view()), #비밀번호 재설정
-    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
     path('emailvalid/', views.emailValid.as_view(), name='email_valid'), # 이메일 중복 확인
-    path('nickNamevalid/', views.nickNameValid.as_view(), name='nickName') # 닉네임 중복 확인
+    path('nickNamevalid/', views.nickNameValid.as_view(), name='nickName'), # 닉네임 중복 확인
 ]
