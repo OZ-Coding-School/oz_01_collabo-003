37ab22060cce019266ce81c63f24b7fc91f05064
Fillsogood <jdd05273@naver.com>
2024-04-16 17:39:14 +0900

    settings 오타 수정

--- backend/config/base.py
+++ backend/config/base.py
@@ -192,7 +192,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "SIGNING_KEY": SECRET_KEY,
     "ALGORITHM" : "HS256",
