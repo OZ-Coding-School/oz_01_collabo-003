b6c1b9c3ee3acb780fbda9d7f6445e8335022cbd
Fillsogood <jdd05273@naver.com>
2024-04-19 11:54:18 +0900

    feat:quiztry delete기능

--- backend/quizs/views.py
+++ backend/quizs/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import QuiztrySerializer
+from quizs.models import QuizTry
 
 class QuizAPIView(APIView):
     def post(self, request, format=None):
@@ -17,3 +19,9 @@ class QuizAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        quiz_try_id = request.data.get('quiz_try_id')
+        quiz_try_instance = get_object_or_404(QuizTry, id=quiz_try_id)
+        quiz_try_instance.delete()
+        return Response({"message": "삭제되었습니다."}, status=status.HTTP_200_OK)
