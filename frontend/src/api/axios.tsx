bc0ae3d4d89c02563b61b2b11f94ac9765ae419d
alsendrha <alsendrha@naver.com>
2024-04-22 02:09:30 +0900

    test 복원

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
